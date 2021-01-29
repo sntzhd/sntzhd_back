feb9135477d5690f8194d5eb6f03a69eba9b9b71
tramX <podwar2008@gmail.com>
2021-01-29 16:45:49 +0200

    fix

---
+++ backend_api/static_data.py
@@ -0,0 +1,7 @@
+sntzhd = dict(name='СНТ \\"ЖЕЛЕЗНОДОРОЖНИК\\"', bank_name='Филиал \\"Центральный\\" Банка ВТБ (ПАО) в г. Москве',
+              bic='044525411', corresp_acc='30101810145250000411', kpp='231201001', payee_inn='2312088371',
+              personal_acc='40703810007550006617', purpose='Оплата электроэнергии по договору №10177', id='0883')
+
+aliases = dict(sntzhd=sntzhd)
+
+url_streets = 'https://next.json-generator.com/api/json/get/N1kZKVgpK'
